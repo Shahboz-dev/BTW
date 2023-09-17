b2736a088386cfbbbc9bee45c46a52d24ebf3f8a
Shahboz <nuriddinovshaxboz1507@gmail.com>
2023-09-17 21:39:32 +0500

    add_gitignore

--- apps/client/static/client/js/clientLoc.js
+++ apps/client/static/client/js/clientLoc.js
@@ -1,6 +1,19 @@
 let map; 
 
+function getClientLoc(){
+    const success = (position) => {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
 
+        return {lat :lat , lng:lng}
+    }
+
+    const error = () => {
+        alert("Error occured")
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error)
+}
 function initMap(){
 
     map = new google.maps.Map(document.getElementById('map'),
